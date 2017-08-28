999bcc22b49bafa6b3c1b20372ceed081a79a952
dalkofjac <dadolg22@gmail.com>
2017-08-28 19:12:48 +0200

    Upload trenutne verzije web stranice
    - dodan sidepanel i footer
    - napravljen početni dizajn stranice
    - napravljene početne verzije index, about i contact HTML-a sa
    pripadajucim CSS-om i JS-om

---
+++ EventTrackerWebApp/public_html/js/site.js
@@ -0,0 +1,15 @@
+// site.sj //
+
+$(document).ready(function() {
+
+    "use strict";
+
+    var btnGit = $("#git-button");
+
+    btnGit.on("click", function(){
+         window.location.href = "https://github.com/dalkofjac/Project-EventTracker";
+         return false;
+    });
+
+});
+
