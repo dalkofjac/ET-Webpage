5b1d3bbdedad2f3ddaa4668796d2b94c785056d1
dalkofjac <dadolg22@gmail.com>
2017-08-28 20:28:24 +0200

    Dodan picture showcase
    - dodana prva verzija picture showcase-a na index.html

--- EventTrackerWebApp/public_html/js/site.js
+++ EventTrackerWebApp/public_html/js/site.js
@@ -10,6 +10,25 @@ $(document).ready(function() {
          window.location.href = "https://github.com/dalkofjac/Project-EventTracker";
          return false;
     });
+    
+    $('#myCarousel').carousel({
+        interval: 40000
+    });
 
+    $('.carousel .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length>0) {
+ 
+        next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');   
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 });
 
